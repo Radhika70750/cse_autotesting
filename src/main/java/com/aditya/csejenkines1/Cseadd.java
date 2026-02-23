83234f68ad8ab84bac2386ba31d537cbb61341dd
Radhika <radhikadevi20052@gmail.com>
2026-02-23 12:33:01 +0530

    commit and push

---
+++ src/main/java/com/aditya/csejenkines1/Cseadd.java
@@ -0,0 +1,10 @@
+package com.aditya.csejenkines1;
+
+public class Cseadd {
+public int add(int a,int b) {
+	return a+b;
+}
+public int mul(int a,int b) {
+	return a*b;
+}
+}
---
+++ src/test/java/com/aditya/csejenkines1/CseaddTest.java
@@ -0,0 +1,19 @@
+package com.aditya.csejenkines1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CseaddTest {
+	Cseadd c=new Cseadd();
+	@Test
+	void Csetest() {
+		int result=c.add(2,3);
+		assertEquals(5,result);
+	}
+	@Test
+	void Csetest1() {
+		int result=c.mul(2,3);
+		assertEquals(6,result);
+	}
+}
